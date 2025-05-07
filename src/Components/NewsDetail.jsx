cc51c06577e6ace8f6adb7a8cfbb7feea56d4163
pradeep <pp0931275@gmail.com>
2025-05-07 22:23:46 +0530

    Updated project files

--- src/Components/NewsDetail.jsx
+++ src/Components/NewsDetail.jsx
@@ -5,7 +5,7 @@ function NewsDetail(props) {
     <div className='w-100 h-100 position-fixed d-flex flex-column justify-content-center bg-dark bg-opacity-75 z-9999' onClick={props.handleDismiss}>
         <button type='button' className="btn border border-secondary text-secondary bg-transparent m-4 ms-auto fs-1 fw-bold">x</button>
         <div className="card m-auto mt-0 p-4" style={ props.mode === 'Dark' ? { backgroundColor : '#666', color : 'white', maxWidth : '90%'} : {maxWidth : '90%'}} onClick={evt => evt.stopPropagation()}>
-                <div className="bg-secondary d-flex justify-content-center align-items-center m-auto" style={{width : "50vh", height : "50vh"}}>
+                <div className="bg-secondary d-flex justify-content-center align-items-center m-auto" style={{width : "50vh", height : "50vh", maxWidth : '100%'}}>
                     <span className="text-white fw-bold" style={{transform : "rotate(-35deg)"}}>Image</span>
                 </div>
             <div className="card-body">
