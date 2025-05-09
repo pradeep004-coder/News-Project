3fb56162e51dea438cf0bd5a03df0a6815c1f095
pradeep <pp0931275@gmail.com>
2025-05-09 12:04:54 +0530

    Updated project files

--- src/Components/NewsDetail.jsx
+++ src/Components/NewsDetail.jsx
@@ -4,7 +4,7 @@ function NewsDetail({handleDismiss, mode, readObj}) {
   return (
     <div  id='newsDetailPage' className='w-100 h-100 position-fixed top-0 start-0 d-flex flex-column justify-content-center bg-dark bg-opacity-75' onClick={handleDismiss}>
         <button type='button' className="btn border border-secondary text-secondary bg-transparent m-4 ms-auto mb-0 fs-1 fw-bold">x</button>
-        <div id='newsDetailCard' className="card w-md-75 m-auto mt-0 p-4" style={ mode === 'Dark' ? { backgroundColor : '#666', color : 'white'} : {}} onClick={evt => evt.stopPropagation()}>
+        <div id='newsDetailCard' className="card w-md-75 m-auto mt-0" style={ mode === 'Dark' ? { backgroundColor : '#666', color : 'white'} : {}} onClick={evt => evt.stopPropagation()}>
                 <div id='newDetailImgPlaceholder' className="bg-secondary d-flex justify-content-center align-items-center m-auto">
                     <span className="text-white fw-bold">Image</span>
                 </div>
